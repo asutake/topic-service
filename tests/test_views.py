5efb52f93d926cade8fa59189c9bfaf2c8582ca0
asutake <asutake.h@gmail.com>
2020-11-04 19:40:36 +0900

    Add test for topic list api

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,5 +1,34 @@
+import json
+
+
 def test_root(client):
     res = client.get('/')
 
     assert 200 == res.status_code
     assert b'Hello World!' in res.data
+
+
+def test_list_topic(client):
+    res = client.get('/topics')
+
+    assert 200 == res.status_code
+    assert [
+        {
+            'id': 1,
+            'title': 'タイトル1',
+            'created_at': '2020-11-04T19:28:38',
+            'deleted_at': None,
+        },
+        {
+            'id': 2,
+            'title': 'タイトル2',
+            'created_at': '2021-11-04T19:28:38',
+            'deleted_at': None,
+        },
+        {
+            'id': 3,
+            'title': 'タイトル3',
+            'created_at': '2022-11-04T19:28:38',
+            'deleted_at': None,
+        },
+    ] == json.loads(res.data)
